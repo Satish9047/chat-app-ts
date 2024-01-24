2b623caa60a23504d0f56e9e1dce2b4a634d51b9
Satish9047 <satishprajapati930@gmail.com>
2024-01-24 21:50:16 +0545

    add config file and port number

---
+++ server/src/config/config.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+    port: process.env.PORT
+};
+
+export default config;
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import config from "./config/config";
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(8080, ()=>{
-    console.log("server is running in port: http://localhost:8080");
+app.listen(config.port, ()=>{
+    console.log(`server is running in port: http://localhost:${config.port}`);
 });
