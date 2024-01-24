1258b8b2ef0a24229cf536e17011f52ee4ae3415
Satish9047 <satishprajapati930@gmail.com>
2024-01-24 21:55:29 +0545

    add cors, morgan, helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import config from "./config/config";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
 
 app.listen(config.port, ()=>{
     console.log(`server is running in port: http://localhost:${config.port}`);
