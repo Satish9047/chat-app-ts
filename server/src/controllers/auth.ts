5c1131e09ed762b2145ee5c2f1acb63f88fc5855
Satish9047 <satishprajapati930@gmail.com>
2024-01-25 15:06:15 +0545

    update: register controller and add: comments

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,26 +1,32 @@
 import { Request, Response } from "express";
 import * as authService from "../services/auth";
 
-
-
+//login controller
 export const loginHandler = async (req: Request, res: Response) => {
     try {
         const userInfo = req.body;
         const data = await authService.loginHandler(userInfo);
-        if("error" in data) return res.status(400).json(data);
+        if ("error" in data) return res.status(400).json(data);
 
-        res.cookie("accessToken", data.accessToken, {httpOnly: true});
-        res.cookie("refreshToken", data.refreshToken, {httpOnly: true});
+        res.cookie("accessToken", data.accessToken, { httpOnly: true });
+        res.cookie("refreshToken", data.refreshToken, { httpOnly: true });
         res.status(200).json(data.msg);
     } catch (error) {
         console.log(error);
-        return res.status(500).json({error: "error while login"});
+        return res.status(500).json({ error: "error while login" });
     }
 
 };
 
+//register controller
 export const registerHandler = async (req: Request, res: Response) => {
-    const userInfo = req.body;
-    const data = await authService.registerHandler(userInfo);
-    res.status(200).json(data);
+    try {
+        const userInfo = req.body;
+        const data = await authService.registerHandler(userInfo);
+        if("error" in data) return res.status(400).json(data);
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "error while register" });
+    }
 };
