3fd26732dcb7307df0f24aeb44a37c339e9c59a1
Satish9047 <satishprajapati930@gmail.com>
2024-01-25 09:22:45 +0545

    add: services for login and register, update: controller

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
+import * as authService from "../services/auth";
 
-export const loginHandler = (req:Request, res:Response)=>{
-    console.log(req.header);
-    res.status(200).json({msg: "hello from the controller"});
+export const loginHandler = async(req:Request, res:Response)=>{
+    const userInfo = req.body;
+    const data = await authService.loginHandler(userInfo);
+    res.status(200).json(data);
 };
 
-export const registerHandler = (req:Request, res:Response)=>{
-    console.log(req.header);
-    res.status(200).json({msg: "hello from the controller"});
+export const registerHandler = async(req:Request, res:Response)=>{
+    const userInfo = req.body;
+    const data = await authService.registerHandler(userInfo);
+    res.status(200).json(data);
 };
---
+++ server/src/services/auth.ts
@@ -0,0 +1,10 @@
+
+export const loginHandler = async(userInfo)=>{
+    console.log(userInfo);
+    return userInfo;
+};
+
+export const registerHandler = (userInfo)=>{
+    console.log(userInfo);
+    return userInfo;
+};
