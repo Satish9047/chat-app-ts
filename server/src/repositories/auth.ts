204374b14a05cd933f9dc7051e3829a8d1dba4b8
Satish9047 <satishprajapati930@gmail.com>
2024-01-25 13:35:58 +0545

    update: register funcationality, register repository

--- server/src/repositories/auth.ts
+++ server/src/repositories/auth.ts
@@ -1,8 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { IRegister } from "../interface/auth";
 
 const prisma = new PrismaClient();
 
-export const getUser = async(username:string)=>{
-    const user = await prisma.user.findFirst({where: {user_name: username}});
+export const getUser = async(email:string)=>{
+    const user = prisma.user.findFirst({where: {email: email}});
+    return user;
+};
+
+export const createUser = async(userInfo:IRegister)=>{
+    const user = await prisma.user.create({
+        data: {
+            user_name: userInfo.username,
+            email: userInfo.email,
+            contact: userInfo.contact,
+            address: userInfo.address,
+            password: userInfo.password
+        }
+    });
     return user;
 };
--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,11 +1,35 @@
+import bcrypt from "bcrypt";
 import { ILogin, IRegister } from "../interface/auth";
+import * as authRepo from "../repositories/auth";
 
-export const loginHandler = async(userInfo:ILogin)=>{
+const saltRounds = 10;
+
+export const loginHandler = async (userInfo: ILogin) => {
     console.log(userInfo);
+    const user = await authRepo.getUser(userInfo.email);
+    if (user) {
+        console.log(user);
+    }
+    console.log("user not found");
+
     return userInfo;
 };
 
-export const registerHandler = async(userInfo:IRegister)=>{
-    console.log(userInfo);
-    return userInfo;
+export const registerHandler = async (userInfo: IRegister) => {
+    const user = await authRepo.getUser(userInfo.email);
+    if (user) {
+        return { error: "user already exist !" };
+    }
+    try {
+        const hashPassword = await bcrypt.hash(userInfo.password, saltRounds);
+        userInfo.password = hashPassword;
+        const newUser = await authRepo.createUser(userInfo);
+        if (!newUser) {
+            return {error: "error while registering user"};
+        }
+        return { msg: "user register successful" };
+    } catch (error) {
+        console.log(error);
+        return {error: "error while registering user"};
+    }
 };
