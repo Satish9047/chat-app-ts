f06ea43a5971c6f456f94e31b3b5c09cb7db6b0f
Satish9047 <satishprajapati930@gmail.com>
2024-01-30 15:08:38 +0545

    update: cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,7 +11,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cookieParser());
