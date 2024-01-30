08227b81225c00e8db4b7e178f0fc6413aae54c4
Satish9047 <satishprajapati930@gmail.com>
2024-01-30 13:42:18 +0545

    add: Route for login, Register, Dashboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
+import { Route, Routes } from "react-router-dom"
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Dashboard from "./pages/dashoard/Dashboard";
 const App: FC = () => {
   return (
     <>
-      <Login />
-      <Register />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
     </>
   );
 };
---
+++ client/src/pages/dashoard/dashboard.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+const Dashboard:FC = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard;
