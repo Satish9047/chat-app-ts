d1e4cc6cc5a52d1096eb1ae06b5a24c5e6ddbf20
Satish9047 <satishprajapati930@gmail.com>
2024-02-09 12:58:05 +0545

    add: axios interceptor for 401 response error

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,8 +1,31 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 const req = axios.create({
     baseURL: "http://localhost:8080/api/v1",
     withCredentials: true,
 })
+
+req.interceptors.response.use(
+    (response: AxiosResponse) => {
+        return response;
+    },
+    async (error: AxiosError) => {
+        if (error.response?.status === 401) {
+            try {
+                const res = await axios.get("http://localhost:8080/api/v1/refresh", {
+                    withCredentials: true,
+                });
+                if (res.status === 200) {
+                    return error.config;
+                }
+
+            } catch (error) {
+                console.log(error);
+                window.location.replace("/src/pages/login/login.tsx");
+            }
+        }
+    }
+)
+
 
 export default req;
